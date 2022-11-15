e2795b2d0868fa38690d2f8aedeaf4ff4ded876c
C-Thanh <116700401+C-Thanh@users.noreply.github.com>
2022-11-15 11:02:17 +0700

    Updated

--- Pictures.h
+++ Pictures.h
@@ -2,213 +2,175 @@
 #ifndef PICTURE
 #define PICTURE
 #include <iostream>
-#include <Windows.h>
 #include <vector>
 using namespace std;
-// for game state
-enum GameState { mainMenu, playMenu, optionMenu, loadGame, playGame, pauseMenu, credits, exitGame };
-enum MovingDir { left_, right_, up_, down_, idle_ };
-
 class picture
 {
-    vector<string> image;
+    vector<wstring> image;
     int width, height;
+
 public:
     picture() {}
-    picture(vector<string> temp);
-    int getWidth() { return width; }
-    int getHeight() { return height; }
-    vector<string> getImage() { return image; }
-};
-void color(int backgroundColor, int text_color);
-void moveToXY(int x, int y);
-void display_img(picture k, int x, int y, int backgroundColor, int TextColor);
-void updatePos(picture image, int x, int y);
-class Position // to be inherited by manything
-{
-public:
-    int x, y;
-    Position() {}
-    Position(int xPos, int yPos)
+    picture(vector<wstring> temp)
     {
-        x = xPos;
-        y = yPos;
+        image = temp;
+        height = temp.size();
+        width = 0;
+        for (auto i : temp)
+            if (i.size() > width)
+                width = i.size();
     }
+    int getWidth() const { return width; } // make sure that they are const
+    int getHeight() const { return height; }
+    vector<wstring> getImg() const { return image; }
 };
-class Animation : public Position
-{
-    int track, cooldown, max_cooldown;
-    int backColor, textColor;
-    MovingDir direction;
 
-public:
-    vector<picture> frames;
-    Animation();
-    Animation(vector<picture> list, int duration, Position posXY, int backgroundColor, int TextColor, MovingDir dir = idle_)
-    {
-        frames = list;
-        max_cooldown = duration;
-        cooldown = 0;
-        x = posXY.x;
-        y = posXY.y;
-        backColor = backgroundColor;
-        textColor = TextColor;
-        track = 0;
-        direction = dir;
-    }
-    void play();
-    picture getCurFrame() { return frames[track]; }
-};
 // this is how we made model or initialized things
 // special type of animation
-const picture car1(vector<string>{
-    "   ____",
-        "__/_|__\\----__",
-        "|(+)-------(+)|"});
+const picture car1(vector<wstring>{
+    L"   ____",
+    L"__/_|__\\----__",
+    L"|(+)-------(+)|"});
 
-const picture car2(vector<string>{
-    "   ____",
-        "__/_|__\\----__",
-        "|(x)-------(x)|"});
+const picture car2(vector<wstring>{
+    L"   ____",
+    L"__/_|__\\----__",
+    L"|(x)-------(x)|"});
 
-const vector<picture> carAnim{ car1, car2 };
+const picture ship1(vector<wstring>{
+    L"     //     ______[_]======   ______",
+    L"____[T]____|__|     |________/     /",
+    L"\\_________________________________/ "});
 
-const picture nguoi1(vector<string>{
-    "(x) ",
-        "/|\\",
-        "/ \\"});
+const picture intro1(vector<wstring>{
+    L" __        __",
+    L"/  \\______/  \\",
+    L"| (.)     (.)|",
+    L"|  ________  |",
+    L"|             |",
+    L"\\____________/"});
 
-const picture ship1(vector<string>{
-    "     //     ______[_]======   ______",
-        "____[T]____|__|     |________/     /",
-        "\\_________________________________/ "});
+const picture intro2(vector<wstring>{
+    L" __        __",
+    L"/  \\______/  \\",
+    L"| (.)     (.)|",
+    L"|  ________  |",
+    L"|  \\______/  |",
+    L"\\____________/"});
 
-const picture intro1(vector<string>{
-    " __        __",
-        "/  \\______/  \\",
-        "| (.)     (.)|",
-        "|  ________  |",
-        "|            |",
-        "\\____________/"});
+const picture carAttack2(vector<wstring>{
+    L"  ==={MG]___",
+    L" ____/__|__\\__",
+    L"|(x)=======(x)|"});
 
-const picture intro2(vector<string>{
-    " __        __",
-        "/  \\______/  \\",
-        "| (.)     (.)|",
-        "|  ________  |",
-        "|    \\__/    |",
-        "\\____________/"});
+const picture bullet1(vector<wstring>{L">=>"});
 
-const vector<picture> introAnim{ intro1, intro2 };
+const picture octo2(vector<wstring>{
+    L"  ________ ",
+    L" /        \\",
+    L" | __  __ |",
+    L" \\        /",
+    L" /        \\",
+    L"/~/\\~/\\~/\\~\\"});
 
-const picture carAttack2(vector<string>{
-    "  ==={MG]___",
-        " ____/__|__\\__",
-        "|(x)=======(x)|"});
+const picture octo1(vector<wstring>{
+    L"  ________ ",
+    L" /        \\",
+    L" | ()  () |",
+    L" \\        /",
+    L" /        \\",
+    L"/_/\\_/\\_/\\_\\"});
 
-const picture bullet1(vector<string>{">=>"});
+const picture pum1(vector<wstring>{
+    L"  ___________ || ___________ ",
+    L" /           \\||/           \\",
+    L"/             \\/             \\",
+    L"|       /\\          /\\       |",
+    L"|      /__\\        /__\\      |",
+    L"|             /\\             |",
+    L"|            /__\\            |",
+    L"|    _________    _______    |",
+    L"\\    \\   __   \\__/ __   /    /",
+    L" \\    \\_/  \\______/  \\_/    /",
+    L"   ------------------------"});
 
-const picture octo2(vector<string>{
-    "  ________ ",
-        " /        \\",
-        " | __  __ |",
-        " \\        /",
-        " /        \\",
-        "/~/\\~/\\~/\\~\\"});
-
-const picture octo1(vector<string>{
-    "  ________ ",
-        " /        \\",
-        " | ()  () |",
-        " \\        /",
-        " /        \\",
-        "/_/\\_/\\_/\\_\\"});
-
-const vector<picture> octoAnim{ octo1, octo2 };
-
-const picture pum1(vector<string>{
-    "  ___________ || ___________ ",
-        " /           \\||/           \\",
-        "/             \\/             \\",
-        "|       /\\          /\\       |",
-        "|      /__\\        /__\\      |",
-        "|             /\\             |",
-        "|            /__\\            |",
-        "|    _________    _______    |",
-        "\\    \\   __   \\__/ __   /    /",
-        " \\    \\_/  \\______/  \\_/    /",
-        "   ------------------------"});
-
-const picture pum2(vector<string>{
-    "  ___________ || ___________ ",
-        " /           \\||/           \\",
-        "/             \\/             \\",
-        "|       /\\          /\\       |",
-        "|      /__\\        /__\\      |",
-        "|             /\\             |",
-        "|            /__\\            |",
-        "|    \\                 /     |",
-        "\\     -----------------      /",
-        " \\      \\/        \\/        /",
-        "   ------------------------"});
-
-const vector<picture> pumAnim{ pum1, pum2 };
-
-const picture m1(vector<string>{
-    " o ",
-        "/|\\",
-        "/ \\"
+const picture pum_2(vector<wstring>{
+    L"  ___________ || ___________ ",
+    L" /           \\||/           \\",
+    L"/             \\/             \\",
+    L"|       /\\          /\\       |",
+    L"|      /__\\        /__\\      |",
+    L"|             /\\             |",
+    L"|            /__\\            |",
+    L"|    \\                 /     |",
+    L"\\     -----------------      /",
+    L" \\      \\/        \\/        /",
+        L"   ------------------------"});
+const picture dog1(vector<wstring>{
+    L"      __",
+    L" (___()ˊˋ",
+    L"/¸    /`",
+    L"//'-- \\\\"
 });
-const picture m2(vector<string>{
-    "\\ o /",
-        "  |  ",
-        " / \\ "
+const picture dog2(vector<wstring>{
+    L"      __",
+    L" (___()ˊˋ",
+    L"/¸    /`",
+    L"\\\\'--//"
 });
-const picture m3(vector<string>{
-    "_ o ",
-        " /\\",
-        "| \\"
+const picture m1(vector<wstring>{
+    L" o ",
+    L"/|\\",
+    L"/ \\"
 });
-const picture m4(vector<string>{
-    "",
-        " ___\\o",
-        "/)  | "
+const picture m2(vector<wstring>{
+    L"\\ o /",
+        L"  |  ",
+        L" / \\ "
 });
-const picture m5(vector<string>{
-    "__|  ",
-        "  \\o ",
-        "  ( \\"
+const picture m3(vector<wstring>{
+    L"_ o ",
+        L" /\\",
+        L"| \\"
 });
-const picture m6(vector<string>{
-    "\\ /",
-        " | ",
-        "/o\\"
+const picture m4(vector<wstring>{
+    L"",
+        L" ___\\o",
+        L"/)  | "
 });
-const picture m7(vector<string>{
-    "   |__",
-        " o/   ",
-        "/ )   "
+const picture m5(vector<wstring>{
+    L"__|  ",
+        L"  \\o ",
+        L"  ( \\"
 });
-const picture m8(vector<string>{
-    "",
-        "o/__ ",
-        "|  (\\"
+const picture m6(vector<wstring>{
+    L"\\ /",
+        L" | ",
+        L"/o\\"
 });
-const picture m9(vector<string>{
-    "o _",
-        "/\\ ",
-        "/ |"
+const picture m7(vector<wstring>{
+    L"   |__",
+        L" o/   ",
+        L"/ )   "
 });
-const vector<picture> man{ m1,m2,m3,m4,m5,m6,m7,m8,m9,m2,m1 };
-
-const picture title(vector<string>{
-    " _____                   _                                    _ ",
-        "/  __ \\                 (_)                                  | |",
-        "| /  \\/_ __ ___  ___ ___ _ _ __   __ _    _ __ ___   __ _  __| |",
-        "| |   | '__/ _ \\/ __/ __| | '_ \\ / _` |  | '__/ _ \\ / _` |/ _` |",
-        "| \\__/\\ | | (_) \\__ \\__ \\ | | | | (_| |  | | | (_) | (_| | (_| |",
-        " \\____/_|  \\___/|___/___/_|_| |_|\\__, |  |_|  \\___/ \\__,_|\\__,_|",
-        "                                  __/ |                        ",
-        "                                 |___/                         "
+const picture m8(vector<wstring>{
+    L"",
+        L"o/__ ",
+        L"|  (\\"
+});
+const picture m9(vector<wstring>{
+    L"o _",
+        L"/\\ ",
+        L"/ |"
+});
+const picture title(vector<wstring>{
+    L" _____                   _                                    _ ",
+        L"/  __ \\                 (_)                                  | |",
+        L"| /  \\/_ __ ___  ___ ___ _ _ __   __ _    _ __ ___   __ _  __| |",
+        L"| |   | '__/ _ \\/ __/ __| | '_ \\ / _` |  | '__/ _ \\ / _` |/ _` |",
+        L"| \\__/\\ | | (_) \\__ \\__ \\ | | | | (_| |  | | | (_) | (_| | (_| |",
+        L" \\____/_|  \\___/|___/___/_|_| |_|\\__, |  |_|  \\___/ \\__,_|\\__,_|",
+        L"                                  __/ |                        ",
+        L"                                 |___/                         "
 });
 #endif
